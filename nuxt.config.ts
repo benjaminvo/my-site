bb1f25d591100c9cd3416b5b31624180821a8fe7
Benjamin Ottensten <benjamin.ottensten@gmail.com>
2023-08-29 10:37:12 +0200

    Remove @nuxt/image and optimize imgs manually

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
-  modules: ["@nuxt/image", "@nuxt/content"],
+  modules: ["@nuxt/content"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
@@ -12,19 +12,6 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    },
-  },
-  image: {
-    // densities: [1, 2, 3],
-    quality: 80,
-    screens: {
-      xs: 320,
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-      xxl: 1536,
-      "2xl": 1536,
     },
   },
 });
--- pages/work.vue
+++ pages/work.vue
@@ -4,7 +4,6 @@
     <!-- <p class="mb-12 sm:mb-20 sm:col-start-2 sm:col-span-14 md:col-start-3 md:col-span-12 lg:col-start-4 lg:col-span-10
       xl:col-start-5 xl:col-span-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum metus
       necmirutrum scelerisque. Cras aliquet libero purus, ut bibendum justo commodo id.</p> -->
-
     <section
       class="mb-40 items-start sm:col-span-8 sm:grid sm:grid-cols-8 sm:gap-x-8 lg:col-span-12 lg:grid-cols-12 xl:col-span-16 xl:grid-cols-16">
       <div
@@ -19,61 +18,71 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 lg:col-start-1 xl:col-span-4">
-        <!-- <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/company-page/mobile-front-page@2x.jpg"
-          quality="100"
-          width="780"
-          height="2248"
-          class="w-full" /> -->
-        <nuxt-img
-          loading="lazy"
-          src="/img/work/company-page/mobile-front-page-552w.jpg"
-          quality="100"
-          width="552"
-          height="1591"
+          src="/img/work/company-page/mobile-front-page.jpg"
+          srcset="
+            /img/work/company-page/mobile-front-page@2x.jpg 2x,
+            /img/work/company-page/mobile-front-page@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/company-page/mobile-position@2x.jpg"
-          quality="100"
+          src="/img/work/company-page/mobile-position.jpg"
+          srcset="
+            /img/work/company-page/mobile-position@2x.jpg 2x,
+            /img/work/company-page/mobile-position@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4">
-        <img loading="lazy" src="/img/work/company-page/mobile-job@2x.jpg" class="w-full" />
-      </div>
-      <div
-        class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/company-page/front-page@2x.jpg"
-          quality="100"
+          src="/img/work/company-page/mobile-job.jpg"
+          srcset="
+            /img/work/company-page/mobile-job@2x.jpg 2x,
+            /img/work/company-page/mobile-job@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/company-page/job@2x.jpg"
-          quality="100"
+          src="/img/work/company-page/front-page.jpg"
+          srcset="
+            /img/work/company-page/front-page@2x.jpg 2x,
+            /img/work/company-page/front-page@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/company-page/position@2x.jpg"
-          quality="100"
+          src="/img/work/company-page/job.jpg"
+          srcset="/img/work/company-page/job@2x.jpg 2x, /img/work/company-page/job@3x.jpg 3x"
+          class="w-full" />
+      </div>
+      <div
+        class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
+        <img
+          loading="lazy"
+          src="/img/work/company-page/position.jpg"
+          srcset="
+            /img/work/company-page/position@2x.jpg 2x,
+            /img/work/company-page/position@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div
         class="border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/company-page/team@2x.jpg"
-          quality="100"
+          src="/img/work/company-page/team.jpg"
+          srcset="/img/work/company-page/team@2x.jpg 2x, /img/work/company-page/team@3x.jpg 3x"
           class="w-full" />
       </div>
     </section>
@@ -91,42 +100,45 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/dashboard/overview@2x.jpg"
-          quality="100"
+          src="/img/work/dashboard/overview.jpg"
+          srcset="/img/work/dashboard/overview@2x.jpg 2x, /img/work/dashboard/overview@3x.jpg 3x"
           class="w-full" />
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/dashboard/org-chart@2x.jpg"
-          quality="100"
+          src="/img/work/dashboard/org-chart.jpg"
+          srcset="/img/work/dashboard/org-chart@2x.jpg 2x, /img/work/dashboard/org-chart@3x.jpg 3x"
           class="w-full" />
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/dashboard/jobs-integrations@2x.jpg"
-          quality="100"
+          src="/img/work/dashboard/jobs-integrations.jpg"
+          srcset="
+            /img/work/dashboard/jobs-integrations@2x.jpg 2x,
+            /img/work/dashboard/jobs-integrations@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/dashboard/jobs-edit@2x.jpg"
-          quality="100"
+          src="/img/work/dashboard/jobs-edit.jpg"
+          srcset="/img/work/dashboard/jobs-edit@2x.jpg 2x, /img/work/dashboard/jobs-edit@3x.jpg 3x"
           class="w-full" />
       </div>
       <div
         class="border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/dashboard/members@2x.jpg"
-          quality="100"
+          src="/img/work/dashboard/members.jpg"
+          srcset="/img/work/dashboard/members@2x.jpg 2x, /img/work/dashboard/members@3x.jpg 3x"
           class="w-full" />
       </div>
     </section>
@@ -148,32 +160,35 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/keyless/landing-page@2x.jpg"
-          quality="100"
+          src="/img/work/keyless/landing-page.jpg"
+          srcset="
+            /img/work/keyless/landing-page@2x.jpg 2x,
+            /img/work/keyless/landing-page@3x.jpg 3x
+          "
           class="w-full" />
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 lg:col-start-1 xl:col-span-4 xl:col-start-5 xl:mb-0">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/keyless/overview@2x.jpg"
-          quality="100"
+          src="/img/work/keyless/overview.jpg"
+          srcset="/img/work/keyless/overview@2x.jpg 2x, /img/work/keyless/overview@3x.jpg 3x"
           class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4 xl:mb-0">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/keyless/photos@2x.jpg"
-          quality="100"
+          src="/img/work/keyless/photos.jpg"
+          srcset="/img/work/keyless/photos@2x.jpg 2x, /img/work/keyless/photos@3x.jpg 3x"
           class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4 xl:mb-0">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/keyless/guide@2x.jpg"
-          quality="100"
+          src="/img/work/keyless/guide.jpg"
+          srcset="/img/work/keyless/guide@2x.jpg 2x, /img/work/keyless/guide@3x.jpg 3x"
           class="w-full" />
       </div>
     </section>
@@ -190,24 +205,27 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 lg:col-start-1 xl:col-span-4 xl:mb-0">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/location/setting@2x.jpg"
-          quality="100"
+          src="/img/work/location/setting.jpg"
+          srcset="/img/work/location/setting@2x.jpg 2x, /img/work/location/setting@3x.jpg 3x"
           class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4 xl:mb-0">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/location/details@2x.jpg"
-          quality="100"
+          src="/img/work/location/details.jpg"
+          srcset="/img/work/location/details@2x.jpg 2x, /img/work/location/details@3x.jpg 3x"
           class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4 xl:mb-0">
-        <nuxt-img loading="lazy" src="/img/work/location/map@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/location/map.jpg"
+          srcset="/img/work/location/map@2x.jpg 2x, /img/work/location/map@3x.jpg 3x"
+          class="w-full" />
       </div>
     </section>
-
     <section
       class="mb-40 items-start sm:col-span-8 sm:grid sm:grid-cols-8 sm:gap-x-8 lg:col-span-12 lg:grid-cols-12 xl:col-span-16 xl:grid-cols-16">
       <div
@@ -226,15 +244,19 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/leasing/business@2x.jpg"
-          quality="100"
+          src="/img/work/leasing/business.jpg"
+          srcset="/img/work/leasing/business@2x.jpg 2x, /img/work/leasing/business@3x.jpg 3x"
           class="w-full" />
       </div>
       <div
         class="border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img loading="lazy" src="/img/work/leasing/car@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/leasing/car.jpg"
+          srcset="/img/work/leasing/car@2x.jpg 2x, /img/work/leasing/car@3x.jpg 3x"
+          class="w-full" />
       </div>
     </section>
     <section
@@ -255,17 +277,33 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 lg:col-start-1 xl:col-span-4">
-        <nuxt-img loading="lazy" src="/img/work/regnsky/1@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/regnsky/1.jpg"
+          srcset="/img/work/regnsky/1@2x.jpg 2x, /img/work/regnsky/1@3x.jpg 3x"
+          class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4">
-        <nuxt-img loading="lazy" src="/img/work/regnsky/2@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/regnsky/2.jpg"
+          srcset="/img/work/regnsky/2@2x.jpg 2x, /img/work/regnsky/2@3x.jpg 3x"
+          class="w-full" />
       </div>
       <div class="mb-8 border border-black/[.15] sm:col-span-4 lg:col-span-4 xl:col-span-4">
-        <nuxt-img loading="lazy" src="/img/work/regnsky/3@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/regnsky/3.jpg"
+          srcset="/img/work/regnsky/3@2x.jpg 2x, /img/work/regnsky/3@3x.jpg 3x"
+          class="w-full" />
       </div>
       <div
         class="border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img loading="lazy" src="/img/work/regnsky/4@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/regnsky/4.jpg"
+          srcset="/img/work/regnsky/4@2x.jpg 2x, /img/work/regnsky/4@3x.jpg 3x"
+          class="w-full" />
       </div>
     </section>
     <section
@@ -285,15 +323,19 @@
       </div>
       <div
         class="mb-8 border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/quasi/article@2x.jpg"
-          quality="100"
+          src="/img/work/quasi/article.jpg"
+          srcset="/img/work/quasi/article@2x.jpg 2x, /img/work/quasi/article@3x.jpg 3x"
           class="w-full" />
       </div>
       <div
         class="border border-black/[.15] sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img loading="lazy" src="/img/work/quasi/search@2x.jpg" quality="100" class="w-full" />
+        <img
+          loading="lazy"
+          src="/img/work/quasi/search.jpg"
+          srcset="/img/work/quasi/search@2x.jpg 2x, /img/work/quasi/search@3x.jpg 3x"
+          class="w-full" />
       </div>
     </section>
     <section
@@ -308,10 +350,10 @@
         </p>
       </div>
       <div class="sm:col-span-8 md:col-span-10 lg:col-span-12 xl:col-start-5">
-        <nuxt-img
+        <img
           loading="lazy"
-          src="/img/work/old-news/concept@2x.jpg"
-          quality="100"
+          src="/img/work/old-news/concept.jpg"
+          srcset="/img/work/old-news/concept@2x.jpg 2x, /img/work/old-news/concept@3x.jpg 3x"
           class="w-full" />
       </div>
     </section>
