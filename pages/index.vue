9f631a238656d61dba3530133126076f7727efef
Benjamin Ottensten <benjamin.ottensten@gmail.com>
2026-02-18 13:28:26 +0100

    Update about

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-  <main
-    class="grid gap-12 sm:col-span-8 sm:gap-14 md:col-start-2 lg:col-start-3 xl:col-span-8 xl:col-start-5">
+  <main class="grid gap-12 sm:col-span-8 sm:gap-14 md:col-start-2 lg:col-start-3 xl:col-span-8 xl:col-start-5">
     <section
       class="w-[calc(100vw-48px)] xs:w-[calc(100vw-80px)] sm:grid sm:w-auto sm:grid-cols-8 sm:items-center sm:gap-8">
       <div class="mb-9 sm:col-span-5 sm:mb-0">
-        <p class="mb-3">
-          I'm an experienced product builder with a background in product, design and frontend.
-        </p>
-        <p>
-          I enjoy building high-quality user experiences that spark joy and drive growth. I have the
-          best experience doing that when the team is small and made up of talented, empathetic and
-          kind people that care about the details.
+        <p class="mb-3">I'm an experienced product builder with a background in product, design and code.</p>
+        <p class="mb-10">
+          I like to lead teams and build high-quality user experiences that bring joy. For the user and for the
+          business. I have the best experience doing that when the team is small and made up of talented people who care
+          about the details.
         </p>
       </div>
       <div class="-ml-6 w-[100vw] overflow-hidden xs:-ml-10 sm:hidden">
@@ -44,17 +41,17 @@
           label="Now"
           title="Open to work"
           subTitle="Copenhagen or Remote"
-          description="I'm currently figuring out what to do next. Freelance, join a startup, start my own company — I'm in exploration mode. Reach out if you want to collaborate." />
+          description="Reach out if you want to collaborate. Freelance, join a startup, start my own thing—I'm in exploration mode." />
         <Block
           label="2023–2026"
           title="Product Lead at Speckle"
-          subTitle="Copenhagen (Remote)"
-          description="Speckle is an open source platform for working with 3D data. It's used by thousands of architects and other professionals within the architecture, engineering and construction industry."
-          description2="I led the development of the web platform and managed the design and frontend teams." />
+          subTitle="Copenhagen (Remote) · Series A · 30 employees"
+          description="Speckle is an open source platform for working with 3D data. It's used by thousands of professionals within the architecture, engineering and construction industry."
+          description2="While I was there I led the development of the web platform, managed the design and frontend teams, and was deeply involved in the successful monetization efforts." />
         <Block
           label="2020–2023"
           title="Head of Product at The Org"
-          subTitle="Copenhagen & New York"
+          subTitle="Copenhagen & New York · Series B · 75 employees"
           description="The Org — backed by Sequoia, Founders Fund and more — is a professional network for companies. I led the product development across multiple teams and managed the PMs."
           description2="While I was there we grew the traffic to 1 million organic visitors/month from 80k, grew contributions from companies to 35k/month from 1k, went from 6 to 75 team members, rebranded, and raised a series B funding round." />
         <Block
@@ -62,16 +59,16 @@
           title="Product Designer at GoMore"
           subTitle="Copenhagen"
           description="GoMore is a Danish peer-to-peer car sharing platform operating in multiple European markets. I did product design for web and apps, frontend development and various PM work."
-          description2="I'm most proud of having led the development of the very popular GoMore Keyless product, which allows car owners to rent out their car to strangers without ever meeting." />
+          description2="I'm most proud of having led the development of the now very popular GoMore Keyless product from start to finish. GoMore Keyless allows car owners to rent out their car to strangers without ever meeting." />
         <Block
           label="2014–2015"
           title="Teaching Assistant at the IT University of Copenhagen"
-          subTitle="Copenhagen · Part-time"
+          subTitle="Copenhagen"
           description="I supervised design projects and conducted weekly exercises for 2nd year bachelor students on the two courses Context-Based App Design and Co-Design." />
         <Block
           label="2012–2013"
           title="Web Designer at Starfly Media"
-          subTitle="Copenhagen · Part-time"
+          subTitle="Copenhagen"
           description="Starfly Media is an online marketing agency. I designed and built landing pages and other sites for various campaigns." />
       </div>
     </section>
@@ -82,16 +79,10 @@
     <section>
       <h2 class="mb-8">Contact</h2>
       <div class="grid gap-3">
-        <Block
-          label="Email"
-          title="benjamin.ottensten@gmail.com"
-          link="mailto:benjamin.ottensten@gmail.com" />
+        <Block label="Email" title="benjamin.ottensten@gmail.com" link="mailto:benjamin.ottensten@gmail.com" />
         <Block label="Phone" title="+45 21952390" link="tel:+4521952390" />
         <Block label="Twitter" title="@benjavo ↗" link="https://www.twitter.com/benjavo" />
-        <Block
-          label="LinkedIn"
-          title="@benjaminvo ↗"
-          link="https://www.linkedin.com/in/benjaminvo" />
+        <Block label="LinkedIn" title="@benjaminvo ↗" link="https://www.linkedin.com/in/benjaminvo" />
         <Block label="Github" title="@benjaminvo ↗" link="https://www.github.com/benjaminvo" />
       </div>
     </section>
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
   bracketSameLine: true,
-  printWidth: 100,
+  printWidth: 120,
 };
