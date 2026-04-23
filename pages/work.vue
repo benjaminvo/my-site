73d6c1275eeca2b956629b5cad349d7a77b1bb03
Benjamin Ottensten <benjamin.ottensten@gmail.com>
2026-04-23 15:00:34 +0200

    Text

--- pages/work.vue
+++ pages/work.vue
@@ -6,7 +6,7 @@
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Transparent employer branding</h2>
-        <small class="text-xs uppercase text-slate-400">The Org, 2023</small>
+        <small class="text-xs text-slate-400 uppercase">The Org, 2023</small>
         <p>
           Every company is competing to hire the best talent. At The Org I helped build an employer branding platform
           where companies attract new talent by being radically transparent about their team and how they work. Because
@@ -21,7 +21,7 @@
         </p>
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0">
         <UnLazyImage
           src-set="
             /img/work/company-page/mobile-front-page.jpg 1x,
@@ -33,7 +33,7 @@
         <Border />
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
         <UnLazyImage
           src-set="
             /img/work/company-page/mobile-position.jpg 1x,
@@ -45,7 +45,7 @@
         <Border />
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
         <UnLazyImage
           src-set="
             /img/work/company-page/mobile-job@2x.jpg 1x,
@@ -56,7 +56,7 @@
           class="w-full" />
         <Border />
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="
             /img/work/company-page/front-page.jpg 1x,
@@ -67,7 +67,7 @@
           class="w-full" />
         <Border />
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/company-page/job.jpg 1x, /img/work/company-page/job@2x.jpg 2x, /img/work/company-page/job@3x.jpg 3x"
           thumbhash="/fcBBQD1mXNrkjfpFmxxafiDORsH"
@@ -98,7 +98,7 @@
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Dashboard</h2>
-        <small class="text-xs uppercase text-slate-400">The Org, 2023</small>
+        <small class="text-xs text-slate-400 uppercase">The Org, 2023</small>
         <p>
           Companies on The Org asked for better user onboarding and an easier way to build their profiles. We built a
           Dashboard to solve those problems. A lot of work went into the integrations that can pull in all relevant data
@@ -116,21 +116,21 @@
           >. My role: Working closely with Design and leading the product development.
         </p>
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/dashboard/overview.jpg 1x, /img/work/dashboard/overview@2x.jpg 2x, /img/work/dashboard/overview@3x.jpg 3x"
           thumbhash="/AcCDQDGynCXWJi3eIzUyMiAdPO2"
           class="w-full" />
         <Border />
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/dashboard/org-chart.jpg 1x, /img/work/dashboard/org-chart@2x.jpg 2x, /img/work/dashboard/org-chart@3x.jpg 3x"
           thumbhash="PQgCBIBCQFZnpYhq1KGkz0gwCA"
           class="w-full" />
         <Border />
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="
             /img/work/dashboard/jobs-integrations.jpg 1x,
@@ -162,7 +162,7 @@ lg:mx-0">
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Keyless car rental</h2>
-        <small class="text-xs uppercase text-slate-400">GoMore, 2019</small>
+        <small class="text-xs text-slate-400 uppercase">GoMore, 2019</small>
         <p>
           GoMore is a platform where people share cars. I led the development of GoMore Keyless which allows car owners
           to rent out their car to strangers without ever meeting.
@@ -174,7 +174,7 @@ lg:mx-0">
         <p>The Keyless hardware is now installed in >1,000 cars across Europe.</p>
         <p>My role: Product management, design, frontend.</p>
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="
             /img/work/keyless/landing-page.jpg 1x,
@@ -186,7 +186,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:mb-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0 xl:col-start-5">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:mb-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/keyless/overview.jpg 1x, /img/work/keyless/overview@2x.jpg 2x, /img/work/keyless/overview@3x.jpg 3x"
           thumbhash="+PcFCwKFg/iKd2Wueoy/t/g"
@@ -194,7 +194,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:mb-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:mb-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
         <UnLazyImage
           src-set="/img/work/keyless/photos.jpg 1x, /img/work/keyless/photos@2x.jpg 2x, /img/work/keyless/photos@3x.jpg 3x"
           thumbhash="+ucFCwTWl0t4f4lexouArAg"
@@ -202,7 +202,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="sm: sm:mx-0col-span-4 relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:mb-0 sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
+        class="sm: sm:mx-0col-span-4 xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:mb-0 sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
         <UnLazyImage
           src-set="/img/work/keyless/guide.jpg 1x, /img/work/keyless/guide@2x.jpg 2x, /img/work/keyless/guide@3x.jpg 3x"
           thumbhash="udcJCwa3iUp4f3c+54tw2gg"
@@ -215,7 +215,7 @@ lg:mx-0">
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Finding your driver</h2>
-        <small class="text-xs uppercase text-slate-400">GoMore, 2018</small>
+        <small class="text-xs text-slate-400 uppercase">GoMore, 2018</small>
         <p>GoMore is also a platform where people share rides with other people heading in the same direction.</p>
         <p>
           One friction point is that it can sometimes be hard to find your driver or passengers when it’s time to meet.
@@ -225,7 +225,7 @@ lg:mx-0">
         <p>My role: Design.</p>
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:mb-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:mb-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0">
         <UnLazyImage
           src-set="/img/work/location/setting.jpg 1x, /img/work/location/setting@2x.jpg 2x, /img/work/location/setting@3x.jpg 3x"
           thumbhash="uucFEgbGgBh4cHIhh4CCBwc"
@@ -233,7 +233,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="sm: sm:mx-0col-span-4 relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:mb-0 sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
+        class="sm: sm:mx-0col-span-4 xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:mb-0 sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
         <UnLazyImage
           src-set="/img/work/location/details.jpg 1x, /img/work/location/details@2x.jpg 2x, /img/work/location/details@3x.jpg 3x"
           thumbhash="uOcFEgZkkFmIZHQieIGAFwg"
@@ -241,7 +241,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="relative mx-auto max-w-[370px] xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
+        class="xs:max-w-[320px] relative mx-auto max-w-[370px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
         <UnLazyImage
           src-set="/img/work/location/map.jpg 1x, /img/work/location/map@2x.jpg 2x, /img/work/location/map@3x.jpg 3x"
           thumbhash="NwgCDAKBB7/XQ7hJlFmKf6z2qA"
@@ -254,7 +254,7 @@ lg:mx-0">
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Colorful car leasing</h2>
-        <small class="text-xs uppercase text-slate-400">GoMore, 2018</small>
+        <small class="text-xs text-slate-400 uppercase">GoMore, 2018</small>
         <p>On GoMore you can lease a car and share it with other people to decrease the monthly lease.</p>
         <p>
           I was the primary designer on everything leasing related. Our goal was to build a leasing platform that
@@ -262,14 +262,14 @@ lg:mx-0">
         </p>
         <p>My role: Design, frontend.</p>
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/leasing/private.jpg 1x, /img/work/leasing/private@2x.jpg 2x, /img/work/leasing/private@3x.jpg 3x"
           thumbhash="+vcFBoKts5d4hqdqh7d2+WSwr3W/SPg"
           class="w-full" />
         <Border />
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/leasing/car.jpg 1x, /img/work/leasing/car@2x.jpg 2x, /img/work/leasing/car@3x.jpg 3x"
           thumbhash="+vcFBoLCvZdTmYifVcZahIl3o2JPK/U"
@@ -289,7 +289,7 @@ lg:mx-0">
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Blog redesign</h2>
-        <small class="text-xs uppercase text-slate-400">Regnsky, 2016</small>
+        <small class="text-xs text-slate-400 uppercase">Regnsky, 2016</small>
         <p>Regnsky was a popular music blog among the Danish indie youth. They asked if I could do a redesign.</p>
         <p>
           We agreed on the keywords Cute, Friendly, Colourful, and Handmade. Seven years later I still think the design
@@ -299,7 +299,7 @@ lg:mx-0">
         <p>My role: Design, frontend.</p>
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0">
+        class="xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:ml-4 lg:col-span-4 lg:ml-0">
         <UnLazyImage
           src-set="/img/work/regnsky/1.jpg 1x, /img/work/regnsky/1@2x.jpg 2x, /img/work/regnsky/1@3x.jpg 3x"
           thumbhash="MbgFCgKVYJhnYnUheI+P1/g"
@@ -307,7 +307,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
+        class="xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:col-span-4 sm:mx-0 sm:max-w-none md:col-span-5 md:mr-4 lg:col-span-4 lg:mr-0">
         <UnLazyImage
           src-set="/img/work/regnsky/2.jpg 1x, /img/work/regnsky/2@2x.jpg 2x, /img/work/regnsky/2@3x.jpg 3x"
           thumbhash="a+cNEgKXdvd3hHKIaI+Ftgk"
@@ -315,7 +315,7 @@ lg:mx-0">
         <Border />
       </div>
       <div
-        class="sm: sm:mx-0col-span-4 relative mx-auto mb-6 max-w-[370px] xs:mb-8 xs:max-w-[320px] sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
+        class="sm: sm:mx-0col-span-4 xs:mb-8 xs:max-w-[320px] relative mx-auto mb-6 max-w-[370px] sm:hidden sm:max-w-none md:col-span-5 lg:col-span-4 lg:block">
         <UnLazyImage
           src-set="/img/work/regnsky/3.jpg 1x, /img/work/regnsky/3@2x.jpg 2x, /img/work/regnsky/3@3x.jpg 3x"
           thumbhash="MxkGCgKWYHhncXQyl4OAGAg"
@@ -335,7 +335,7 @@ lg:mx-0">
       <div
         class="mb-10 grid gap-3 sm:col-span-8 md:col-start-2 lg:col-start-3 lg:mb-14 xl:sticky xl:top-16 xl:col-span-4 xl:col-start-1 xl:mb-0">
         <h2>Motivating students to read</h2>
-        <small class="text-xs uppercase text-slate-400">Research, 2016</small>
+        <small class="text-xs text-slate-400 uppercase">Research, 2016</small>
         <p>
           I was not the most motivated student. That’s why I decided to build Quasi with Simon Lind and Andreas
           Reffstrup to see if we could build a platform that motivated students to read more academic literature.
@@ -348,7 +348,7 @@ lg:mx-0">
         </p>
         <p>My role: Design, research.</p>
       </div>
-      <div class="relative mb-6 xs:mb-8 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
+      <div class="xs:mb-8 relative mb-6 sm:col-span-8 md:col-span-10 md:mx-4 lg:col-span-12 lg:mx-0 xl:col-start-5">
         <UnLazyImage
           src-set="/img/work/quasi/article.jpg 1x, /img/work/quasi/article@2x.jpg 2x, /img/work/quasi/article@3x.jpg 3x"
           thumbhash="/fcBBIDzmKdpdph/hbqxVhD7Jw"
@@ -389,7 +389,7 @@ lg:mx-0">
     <a
       class="col-span-full mx-auto block cursor-pointer text-center text-slate-400 hover:no-underline"
       @click="scrollToTop"
-      >↑<br />Scroll to top</a
+      >↑<br />Back to top</a
     >
   </main>
 </template>
