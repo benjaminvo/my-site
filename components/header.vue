f70ab57f6a498b2e5ac0d1f1519564540a3dee6b
Benjamin Ottensten <benjamin.ottensten@gmail.com>
2026-05-26 12:20:37 +0200

    Update home link

--- components/header.vue
+++ components/header.vue
@@ -2,15 +2,13 @@
   <header
     class="xs:flex xs:justify-between mb-12 content-center sm:col-span-8 sm:mb-20 md:col-start-2 lg:col-start-3 xl:col-start-5">
     <div class="mb-8 sm:mb-0">
-      <h1>
-        <NuxtLink
-          to="/"
-          class="tap-highlight-none no-underline outline-none ring-0 select-none hover:no-underline focus:outline-none focus-visible:outline-none focus:ring-0 focus-visible:ring-0 dark:text-white">
-          Benjamin Ottensten
-        </NuxtLink>
-      </h1>
-      <div class="text-slate-400">Product Lead & Builder</div>
-      <div class="text-slate-400">Copenhagen, Denmark</div>
+      <NuxtLink
+        to="/"
+        class="tap-highlight-none block no-underline outline-none ring-0 select-none hover:no-underline focus:outline-none focus-visible:outline-none focus:ring-0 focus-visible:ring-0 dark:text-white">
+        <h1>Benjamin Ottensten</h1>
+        <div class="text-slate-400">Product Lead & Builder</div>
+        <div class="text-slate-400">Copenhagen, Denmark</div>
+      </NuxtLink>
     </div>
     <Navigation />
   </header>
